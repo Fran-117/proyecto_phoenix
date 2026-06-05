60f493b353b18f63034694accfd1e410b90a5ac4
Fran-117 <27francis3@gamil.com>
2026-06-05 17:15:10 -0600

    Se uso el comando "mix phx.gen.auth Accounts User users" para generar el código de autenticación de usuarios. Luego se tradujeron algunos textos al español.

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :proyecto_phoenix, :scopes,
+  user: [
+    default: true,
+    module: ProyectoPhoenix.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: ProyectoPhoenix.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :proyecto_phoenix,
   ecto_repos: [ProyectoPhoenix.Repo],
   generators: [timestamp_type: :utc_datetime]
